9acbccf52a4d459bb3778da5fc9db3f74a4334a8
nikhil003 <nikhil.enabothulaa@gmail.com>
2024-01-28 01:28:51 +0530

    added one negative case

--- CYPressAuto/cypress/e2e/spec2.cy.js
+++ CYPressAuto/cypress/e2e/spec2.cy.js
@@ -1,13 +1,20 @@
 describe('test1',()=>{
 
-    it('test11',()=>{
+    it('Postive case',()=>{
         cy.visit("https://institute.leo1.in/institute/login")
         cy.get("input#formEmail").type("siddharth.bidwan@leo1.in")
         cy.get("input#formpassword").type("lw@123")
         cy.get("button[type='submit']").click()
         cy.get("div.col-lg-9").contains("Welcome, Siddharth Bidwan!")
-
         
+    })
+    it('Negative case',()=>{
+        cy.visit("https://institute.leo1.in/institute/login")
+        cy.get("input#formEmail").type("siddharth.bidwan@leo1.in")
+        cy.get("input#formpassword").type("lw@123")
+        cy.get("button[type='submit']").click()
+        cy.get("div.col-lg-9").contains("Exit, Siddharth Bidwan!")
+
     })
 
 }
