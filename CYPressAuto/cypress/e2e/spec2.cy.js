a47416c34aedfb6ad947550bb017c442159888a1
nikhil003 <nikhil.enabothulaa@gmail.com>
2024-01-28 00:48:06 +0530

    code updated with institute dashboard

--- CYPressAuto/cypress/e2e/spec2.cy.js
+++ CYPressAuto/cypress/e2e/spec2.cy.js
@@ -1,12 +1,13 @@
-const cypress = require("cypress")
-
-
 describe('test1',()=>{
 
     it('test11',()=>{
         cy.visit("https://institute.leo1.in/institute/login")
-        cy.title().should('eq','Institute Login - LEO1')
-        cypress.title().should('eq', 'Insta')
+        cy.get("input#formEmail").type("siddharth.bidwan@leo1.in")
+        cy.get("input#formpassword").type("lw@123")
+        cy.get("button[type='submit']").click()
+        cy.get("div.col-lg-9").contains("Welcome, Siddharth Bidwan!")
+
+        
     })
 
 }
